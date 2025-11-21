fe2392a76be59545301bb3ef4e9270810151f824
GupaloDariaR <135831301+GupaloDariaR@users.noreply.github.com>
2025-11-22 00:16:35 +0300

    added method printThreeWords

---
+++ src/Lesson_2/App.java
@@ -0,0 +1,10 @@
+package Lesson_2;
+
+import static Lesson_2.Methods.printThreeWords;
+
+public class App {
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+}
---
+++ src/Lesson_2/Methods.java
@@ -0,0 +1,11 @@
+package Lesson_2;
+
+public class Methods {
+    public static void printThreeWords() {
+//  Выводит в столбец три слова: Orange, Banana, Apple
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
