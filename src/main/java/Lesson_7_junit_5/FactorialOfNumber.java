ec927e0a96214a9ab570641690f2d1029c8490a4
GupaloDariaR <135831301+GupaloDariaR@users.noreply.github.com>
2025-12-12 03:18:15 +0300

    change FactorialOfNumber

--- src/main/java/Lesson_7_junit_5/FactorialOfNumber.java
+++ src/main/java/Lesson_7_junit_5/FactorialOfNumber.java
@@ -2,7 +2,16 @@ package main.java.Lesson_7_junit_5;
 
 public class FactorialOfNumber {
 
-    public static long getFactorialOfNumber(int number) {
+    public static long getFactorialOfNumber(int number) throws IllegalArgumentException{
+
+        if (number < 0) {
+            throw new IllegalArgumentException("Факториал отрицателного числа не определен");
+        }
+
+        if (number == 0) {
+            return 1;
+        }
+
         long result = 1;
         for (int i = 1; i <= number; i++) {
             result *= i;
